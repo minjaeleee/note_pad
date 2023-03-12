a9c9e30ffe9a6be2c818719bafeeb7ec7814de30
minjaeleee <dlalswo4602@naver.com>
2023-03-12 15:43:14 +0900

    관리자 페이지 기능 수정 작업 완료

--- client/src/pages/manager/MemoManagerPage.tsx
+++ client/src/pages/manager/MemoManagerPage.tsx
@@ -14,8 +14,6 @@ const MemoManagerPage = () => {
   const [memoList, setMemoList] = useState<Memo[]>([])
   const [selectedMemoList, setSelectedMemoList] = useState<string[]>([])
 
-
-    // 임시 저장된 값을 불러옴
     useEffect(()=> {
       loadMemo()
     },[])
@@ -24,6 +22,40 @@ const MemoManagerPage = () => {
       const {data} = await axios.get<Memo[]>('/')
       setMemoList(data)
     },[setMemoList])
+
+    const resetSelectedList = () => {
+      setSelectedMemoList([])
+    }
+
+    const selectList = useCallback(value=>{
+      setSelectedMemoList(prev => {
+        if (prev.includes(value.id))
+          return prev.filter(v => value.id !== v)
+        return [...prev, value.id]
+        })
+    },[setSelectedMemoList,])
+
+    const removeSelectedList = useCallback(async()=>{
+      const list = []
+      for (const id of selectedMemoList) {
+        // await axios.delete("/"+id)
+          list.push(axios.delete("/" + id))
+      }
+      await Promise.all(list)  
+      await loadMemo()
+      resetSelectedList()
+      alert("제거 완료!")
+    },[selectedMemoList, loadMemo])
+
+    const removeAllList = useCallback(async()=>{
+      if (!window.confirm("정말로 전체 제거를 하시겠습니까?")) {
+        return;
+      }
+      await axios.delete("/")  
+      await loadMemo()
+      alert("제거 완료!")
+      resetSelectedList()
+    },[loadMemo])
 
   return (
     <div className={styles.wrapper}>
@@ -55,28 +87,14 @@ const MemoManagerPage = () => {
           <>
             <button 
               className={styles.removedSelectedBtn}
-              onClick={async () => {
-              const list = []
-              for (const id of selectedMemoList) {
-                // await axios.delete("/"+id)
-                  list.push(axios.delete("/" + id))
-              }
-              await Promise.all(list)  
-              await loadMemo()
-              alert("제거 완료!")
-            }}>
+              onClick={removeSelectedList}
+            >
               선택 제거
             </button>
             <button 
               className={styles.allRemovedBtn}
-              onClick={async () => {
-              if (!window.confirm("정말로 전체 제거를 하시겠습니까?")) {
-                return;
-              }
-              await axios.delete("/")  
-              await loadMemo()
-              alert("제거 완료!")
-            }}>
+              onClick={removeAllList}
+            >
               전체 제거
             </button>
           </>
@@ -87,16 +105,9 @@ const MemoManagerPage = () => {
           memoList.map(value => {
             return (
               <div
-              key={value.created_at}
-              className={styles.list}
-              onClick={()=> {
-                setSelectedMemoList(prev => {
-                  if (prev.includes(value.id))
-                    return prev.filter(v => value.id !== v)
-                  return [...prev, value.id]
-                  })
-                }
-              }
+                key={value.created_at}
+                className={(selectedMemoList.join().includes(value.id)) ? styles.selectedList : styles.list}
+                onClick={()=>selectList(value)}
               >
             <div
               className={styles.content}
